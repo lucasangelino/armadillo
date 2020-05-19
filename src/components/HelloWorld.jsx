5479f53f95df2dd640ef87dcb13b7bb65f708e15
= <angelino.n.lucas@gmail.com>
2020-05-19 11:32:23 -0300

    eslint config

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const HelloWorld = () => {
+    return (
+        <h1>Hello World</h1>
+    );
+};
+
+export default HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// componenetes
+import HelloWorld from './components/HelloWorld.jsx';
+
+ReactDom.render(<HelloWorld/>, document.getElementById('app'));
+
